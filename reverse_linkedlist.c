12f0d17b2c3794e2cac6198765a81596fd7af778
sathishintel <jsathishkumarc@gmail.com>
2017-06-07 19:37:05 +0530

    Update reverse_linkedlist.c

--- reverse_linkedlist.c
+++ reverse_linkedlist.c
@@ -11,3 +11,19 @@ while(head)
 	head=temp;
 
 }
+
+//second method
+struct node* recursiveReverseLL(struct node* first){
+
+   if(first == NULL) return NULL; // list does not exist.
+
+   if(first->link == NULL) return first; // list with only one node.
+
+   struct node* rest = recursiveReverseLL(first->link); // recursive call on rest.
+
+   first->link->link = first; // make first; link to the last node in the reversed rest.
+
+   first->link = NULL; // since first is the new last, make its link NULL.
+
+   return rest; // rest now points to the head of the reversed list.
+}
