e6c0b42e540796bf09c098944f17a3b2fa075d1b
sathish Jarugumalli <jsathishkumarc@gmail.com>
2022-06-30 21:25:50 +0530

    Create palindrome_sll.c

---
+++ palindrome_sll.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int __is_palindrome(struct node *left , struct node *right) {
+ if (right ==  NULL)
+     return 1;
+  }
+  int flag = __is_palindrom(left,right);
+  if (flag == 0) 
+    return 0;
+
+  int flag2 = (right->data == left->data);
+  left = left->next;
+  return flag2;
+}
+
+int is_palindrome(strcy node *head) {
+    int pal = __is_palindrome(head,head);
+    if (pal != 1)
+        printf("not a palindrome\n");
+}
