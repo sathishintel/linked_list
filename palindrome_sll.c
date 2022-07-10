454e7e7fd84de90b8ec7211f6b1d33b188d83348
sathish Jarugumalli <jsathishkumarc@gmail.com>
2022-07-10 23:03:48 +0530

    Update palindrome_sll.c

--- palindrome_sll.c
+++ palindrome_sll.c
@@ -19,3 +19,38 @@ int is_palindrome(strcy node *head) {
     if (pal != 1)
         printf("not a palindrome\n");
 }
+
+
+
+Second Method without recursion
+//isPalindrome() will determine whether given list is palindrome or not.  
+void isPalindrome(){  
+    struct node *current = head;  
+    bool flag = true;  
+      
+    //Store the mid position of the list  
+    int mid = (size%2 == 0)? (size/2) : ((size+1)/2);  
+      
+    //Finds the middle node in given singly linked list  
+    for(int i=1; i<mid; i++){  
+        current = current->next;  
+    }  
+      
+    //Reverse the list after middle node to end  
+    struct node *revHead = reverseList(current->next);  
+   
+    //Compare nodes of first half and second half of list  
+    while(head != NULL && revHead != NULL){  
+        if(head->data != revHead->data){  
+            flag = false;  
+            break;  
+        }  
+        head = head->next;  
+        revHead = revHead->next;  
+    }  
+   
+    if(flag)  
+        printf("Given singly linked list is a palindrome\n");  
+    else  
+        printf("Given singly linked list is not a palindrome\n");  
+}  
